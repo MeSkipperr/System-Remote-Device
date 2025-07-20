df1dc9247a4fe7e4f98484988e551b5125f31218
MeSkipperr <ikadekyolaandika02@gmail.com>
2025-07-21 01:10:07 +0800

    fix : change count error to times if arp not found

--- project/monitoring-network.go
+++ project/monitoring-network.go
@@ -165,6 +165,15 @@ func PingDevice(dev models.DeviceType, conf monitoringNetworkType) {
 
 	if !valueARP.Status && !dev.Error {
 		sendErrorEmail(dev, conf, linesResArray)
+		
+		countBol, countMessage := updateCountError(dev, times)
+
+		if !countBol {
+			if errLog := utils.WriteFormattedLog(conf.LogPath, "ERROR", "database", fmt.Sprintf("Error query to database: %v", countMessage)); errLog != nil {
+				fmt.Printf("Failed to write log: %v\n", errLog)
+			}
+		}
+
 		return
 	}
 
